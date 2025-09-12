8a7a221d301cda9af397d404afc1db89a6875e2b
ellaccamp <ella.c.camp-1@ou.edu>
2025-09-12 10:03:58 -0500

    homework 1 sales

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Ella Camp
+// Did you seek help ? If yes, specify the helper or web link here: Max Ngo & Pooja (MIS helplab)
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,34 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            //asking questions
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            string product = Console.ReadLine();
+            Console.WriteLine("How many football tickets do you want to buy?");
+            int quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the price for each football ticket?");
+            double price = double.Parse(Console.ReadLine());
+
+            //subtotal
+            double subtotal = price * quantity;
+            subtotal = Math.Round(subtotal, 2);
+            Console.WriteLine("The subtotal for your bill is $" + subtotal);
+
+            //sales tax
+            double taxrate = 0.085;
+            double salestax = subtotal * taxrate;
+            salestax = Math.Round(salestax, 2);
+            Console.WriteLine("The sales tax for your bill is $" + salestax);
+
+            //sales total
+            double salestotal = subtotal + salestax;
+            salestotal = Math.Round(salestotal, 2);
+            Console.WriteLine("The total for your bill $" + salestotal);
+
+            Console.WriteLine("Press any key to continue...");
+
+
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1b Grade
 
-// Your Name:
+// Your Name: Ella Camp
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,46 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            //student info
+            Console.WriteLine("What is your first name?");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("What is your student ID?");
+            int studentid = int.Parse(Console.ReadLine());
+
+            //grade questions
+            Console.WriteLine("What is your overall percentage grade for homework?");
+            double homework = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for participation?");
+            double part = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            double quiz = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            double midterm = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            double final = Convert.ToDouble(Console.ReadLine());
+
+            //establish %
+            double homeworkw = 0.20;
+            double partw = 0.15;
+            double quizw = 0.15;
+            double midtermw = 0.25;
+            double finalw = 0.25;
+
+            //calculate final grade
+            double finalgrade = (homework*homeworkw) + (part*partw) + (quiz*quizw) + (midterm*midtermw) + (final*finalw);
+
+            Console.WriteLine($"{firstname} {lastname} ({studentid}), your final grade is {finalgrade}%");
+
+            Console.WriteLine("Press any key to continue...");
+
+
+
+
+
+
+
         }
     }
 }
